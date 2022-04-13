41fe32592c7d5d325629643d6e7f3b441128120d
Vitoria-m <vitoriazzz@outlook.com>
2022-04-13 17:14:34 -0300

    counter

---
+++ day3.js
@@ -0,0 +1,11 @@
+let num = document.getElementById('number').value = 0
+
+function plus() {
+    num ++ 
+    document.getElementById('number').innerHTML = num     
+}
+
+function minus() {
+    num --
+    document.getElementById('number').innerHTML = num 
+}
